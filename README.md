c7795296b800676da4fe81d08429fae93d7393c4
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-10-23 15:17:45 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,25 @@
+# ddc-ui-none
+
+None UI for ddc.vim
+
+This UI does not display items.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddc.vim
+
+https://github.com/Shougo/ddc.vim
+
+### none.vim
+
+https://github.com/Shougo/none.vim
+
+## Configuration
+
+```vim
+call ddc#custom#patch_global('ui', 'none')
+```
---
+++ denops/@ddc-uis/none.ts
@@ -0,0 +1,25 @@
+import { Context, DdcItem } from "https://deno.land/x/ddc_vim@v3.0.0/types.ts";
+import { BaseUi } from "https://deno.land/x/ddc_vim@v3.0.0/base/ui.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v3.0.0/deps.ts";
+
+export type Params = Record<never, never>;
+
+export class Ui extends BaseUi<Params> {
+  async show(_args: {
+    denops: Denops;
+    context: Context;
+    completePos: number;
+    items: DdcItem[];
+    uiParams: Params;
+  }): Promise<void> {
+  }
+
+  async hide(_args: {
+    denops: Denops;
+  }): Promise<void> {
+  }
+
+  params(): Params {
+    return {};
+  }
+}
