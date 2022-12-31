cf4c7bc9b68041088cfcc835b9dac9cf6f4be771
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-12-31 16:11:21 +0900

    Update

--- denops/@ddc-uis/none.ts
+++ denops/@ddc-uis/none.ts
@@ -1,5 +1,5 @@
-import { BaseUi } from "https://deno.land/x/ddc_vim@v3.2.0/base/ui.ts";
-import { Denops } from "https://deno.land/x/ddc_vim@v3.2.0/deps.ts";
+import { BaseUi } from "https://deno.land/x/ddc_vim@v3.4.0/base/ui.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v3.4.0/deps.ts";
 
 export type Params = Record<never, never>;
 
@@ -12,6 +12,13 @@ export class Ui extends BaseUi<Params> {
   override async hide(_args: {
     denops: Denops;
   }): Promise<void> {
+  }
+
+  // deno-lint-ignore require-await
+  override async visible(_args: {
+    denops: Denops;
+  }): Promise<boolean> {
+    return false;
   }
 
   override params(): Params {
